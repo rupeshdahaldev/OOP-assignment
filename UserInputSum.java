e1c2d576d5f73320d424a8ea5eb87f49663d03f6
Rupesh Dahal <rupeshdahal.dev@gmail.com>
2025-12-10 23:41:18 +0545

    Add files via upload

---
+++ UserInputSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner ;
+public class UserInputSum {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Number");
+    int num1 = sc.nextInt();
+
+        System.out.print("Enter SECOND Number");
+    int num2 = sc.nextInt();
+
+    int sum = num1 + num2;
+
+        System.out.println("sum of two numners are = "+sum);
+  }
+}
+
---
+++ VariablesDemo.java
@@ -0,0 +1,16 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double height = 5.9;
+        boolean isStudent = true;
+        char grade = 'A';
+        String name = "Rupesh";
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Grade: " + grade);
+        System.out.println("Name: " + name);
+    }
+}
